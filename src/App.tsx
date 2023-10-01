3980610ed70113948895ba7f935b50c8d61d5c4f
ayakotanaka0224 <ayakosomethingxxx@gmail.com>
2023-10-02 07:39:06 +0900

    下準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Container>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +19,47 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Container>
   );
-}
+};
 
 export default App;
+
+// styled-componentsというライブラリを用いてこのようにcssを記述することができる
+const Container = styled.div`
+  text-align: center;
+  .App-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+
+  .App-link {
+    color: #61dafb;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
---
+++ src/asset/theme.ts
@@ -0,0 +1,6 @@
+const colors = {
+  white: "#fff",
+  blue: "#2791d4",
+};
+
+export default colors;

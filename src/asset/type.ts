ad96359255aa257ff3a21fcdaa80a6a0da4f8a2f
ayakotanaka0224 <ayakosomethingxxx@gmail.com>
2023-10-03 23:29:44 +0900

    ロジックの仮動作

--- src/asset/type.ts
+++ src/asset/type.ts
@@ -1,1 +1,3 @@
 export type Choice = { text: string; value: number };
+
+export type Question = [{ title: string }, Choice[]];
--- src/component/QA.tsx
+++ src/component/QA.tsx
@@ -1,13 +1,11 @@
-import React, { memo } from "react";
+import { memo } from "react";
 import styled from "styled-components";
-import colors from "../asset/theme";
-import { Choice } from "../asset/type";
+import { Choice, Question } from "../asset/type";
 import Button from "./Button";
 
 interface QAProps {
-  question: string;
-  choices: Choice[];
-  onClick: () => void;
+  question: Question;
+  onClick: (value: Choice) => void;
   onClickPrevious: () => void;
   onClickTop: () => void;
 }
@@ -15,16 +13,50 @@ interface QAProps {
 const QA = memo((props: QAProps) => {
   return (
     <Container>
-      <p>{props.question}</p>
-      {props.choices.map((choice) => {
-        return <Button text={choice.text} onClick={props.onClick} />;
-      })}
-      <Button text="前へ" onClick={props.onClickPrevious} />
-      <Button text="トップへ戻る" onClick={props.onClickTop} />
+      <h2>{props.question[0]["title"]}</h2>
+      <ButtonWrapper>
+        {props.question[1].map((choice) => {
+          return (
+            <Button text={choice.text} onClick={() => props.onClick(choice)} />
+          );
+        })}
+      </ButtonWrapper>
+      <BackButtonWrapper>
+        <Button text="前へ" onClick={props.onClickPrevious} />
+        <Button text="トップへ戻る" onClick={props.onClickTop} />
+      </BackButtonWrapper>
     </Container>
   );
 });
 
 export default QA;
 
-const Container = styled.div``;
+// styled-componentsというライブラリを用いてこのようにcssを記述することができる
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  h2 {
+    text-align: center;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  button {
+    width: 45%;
+  }
+`;
+
+const BackButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  button {
+    width: 30%;
+  }
+`;
